990271c3ec623e4b9ea6c58923c6ac172873e76c
michailidm <72384900+michailidm@users.noreply.github.com>
2023-12-18 17:42:53 +0200

    Delete src directory

--- src/main/java/calculator/TrigonometricOperation.java
+++
@@ -1,7 +0,0 @@
-package calculator;
-
-/**
- * The <code>TrigonometricOperation</code> class represents a trigonometric operation.
- */
-abstract public class TrigonometricOperation extends UnaryOperation {
-}
